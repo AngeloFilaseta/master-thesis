57e2ab165d80934e5d526c7e8e3fbd396a85744a
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-03-07 10:53:17 +0100

    style: fix listing and image for page organization

--- listings/SerializationModuleExample.kt
+++ listings/SerializationModuleExample.kt
@@ -1,8 +1,10 @@
 val format = Json { serializersModule = concentrationModule }
 val concentration: Any =
     ConcentrationSurrogateMock(name = "example")
-println(format.encodeToString(
-    PolymorphicSerializer(Any::class), concentration)
+println(
+    format.encodeToString(
+        PolymorphicSerializer(Any::class), concentration
+    )
 )
 //OUTPUT {
 //    "type":"ConcentrationSurrogateMock",
